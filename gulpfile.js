7f7e9b2ba0521534c17dbaf04f79aacd9c08af9a
Mario Hernandez <mario.hernandez@mediacurrent.com>
2023-11-10 21:40:19 -0800

    Updates tulp tasks and postcss tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const {parallel, watch} = require('gulp');
 
 // Pull in each task
 const images = require('./gulp-tasks/images.js');
-const sass = require('./gulp-tasks/compile.js');
+// const sass = require('./gulp-tasks/compile.js');
 const { manifest } = require('./gulp-tasks/move.js');
 
 // Set each directory and contents that we want to watch and
@@ -13,13 +13,33 @@ const { manifest } = require('./gulp-tasks/move.js');
 // will run when a file changes.
 const watcher = () => {
   watch('./src/images/**/*', {ignoreInitial: true}, images);
-  watch('./src/scss/**/*.scss', {ignoreInitial: true}, sass);
+  // watch('./src/scss/**/*.scss', {ignoreInitial: true}, sass);
   watch('./src/*.webmanifest', {ignoreInitials: true}, manifest);
 };
 
 // The default (if someone just runs `gulp`) is to run each task in parrallel
-exports.default = parallel(images, sass, manifest);
+// exports.default = parallel(images, sass, manifest);
+exports.default = parallel(images, manifest);
 
 // This is our watcher task that instructs gulp to watch directories and
 // act accordingly
 exports.watch = watcher;
+
+// The task below watches for css changes using postcss.
+// const {src, dest, watch} = require ('gulp');
+// var postcss = require ('gulp-postcss');
+
+// function buildCSS(cb) {
+//   src('./src/css/default.css')
+//   .pipe(postcss())
+//   .pipe(dest('./dist/css'))
+//   cb();
+// }
+
+// function watchFiles(cb) {
+//   watch(['./src/css/**/*.css'], buildCSS)
+//   cb();
+// }
+
+// exports.buildCSS = buildCSS;
+// exports.default = watchFiles;
