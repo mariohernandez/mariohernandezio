d5089b37d8d16380dafcb7338783c90494687d8f
mariohernandez <mrhernandez@mednet.ucla.edu>
2023-12-10 13:31:59 -0800

    Updates images.js gulp task

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -1,32 +1,32 @@
 'use strict';
 
-const {dest, src} = require('gulp');
-const gulpImagemin = import('gulp-imagemin');
-// const imagemin = require('gulp-imagemin');
+// Include gulp
+const { src, dest } = require('gulp');
 
-// Grabs all images, runs them through imagemin
-// and plops them in the dist folder
-const images = () => {
-  // We have specific configs for jpeg and png files to try
-  // to really pull down asset sizes
-  return src('./src/images/**/*')
-    .pipe(
-      gulpImagemin(
-        [
-          gulpImagemin.gifsicle({interlaced: true}),
-          gulpImagemin.mozjpeg({quality: 75, progressive: true}),
-          gulpImagemin.optipng({optimizationLevel: 5}),
-          imagemin.optipng({optimizationLevel: 5, interlaced: null})
-        ],
-        {
-          silent: true
-        }
-      )
+// Include Our Plugins
+const move = require('gulp-rename');
+
+// Export our tasks.
+module.exports = {
+  // Move any fonts to where Pattern Lab is lookinging for them.
+  moveImages: function() {
+    return src(
+      [
+        './src/images/**/*.gif',
+        './src/images/**/*.png',
+        './src/images/**/*.jpg',
+        './src/images/**/*.jpeg',
+        './src/images/**/*.svg',
+        './src/images/**/*.webp'
+      ],
+      { base: './' }
     )
-    .pipe(dest('./dist/images'))
-    .on('end', () => {
-     console.log('Successfully compressed images');
-  });
+      .pipe(
+        move(function(path) {
+          path.dirname = '';
+          return path;
+        })
+      )
+      .pipe(dest('./dist/images'));
+  }
 };
-
-module.exports = images;
