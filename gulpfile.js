868fc963e4e78e71434bc026e98219914a115c57
mariohernandez <mrhernandez@mednet.ucla.edu>
2023-12-10 16:39:44 -0800

    Updates gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@
 const { src, dest, series, watch } = require('gulp');
 const squoosh = require('gulp-libsquoosh');
 
-// Copy images into dist/images
-function copyImages() {
+// Copies images from src to dist on demand.
+function copyImages(cb) {
   return src('src/images/**')
     .pipe(squoosh())
     .pipe(dest('dist/images'));
+    console.log('Helloooooo world!');
+  cb();
 };
 
-exports.default = series(copyImages);
-exports.watch = series(copyImages);
+exports.default = copyImages;
