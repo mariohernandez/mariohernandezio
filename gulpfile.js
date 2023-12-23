3af49edfd3155b6fcfa710c0cd986221485fca8f
mariohernandez <mrhernandez@mednet.ucla.edu>
2023-12-22 22:41:32 -0800

    Adds Wotfard webfonts to the site

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
-const { src, dest } = require("gulp");
+const { src, dest, series, watch } = require("gulp");
 
 function copyImages(cb) {
-   src('./src/images/**/*')
-        .pipe(dest('./dist/images'));
-   cb();
+  src('./src/images/**/*')
+    .pipe(dest('./dist/images'));
+  cb();
 }
-exports.default = copyImages;
+
+function copyFonts(cb) {
+  src('./src/fonts/*')
+    .pipe(dest('./dist/fonts'));
+  cb();
+}
+
+function copyCSS(cb) {
+  src('./src/css/**/*')
+    .pipe(dest('./dist/css'));
+  cb();
+}
+
+exports.default = series(copyImages, copyFonts, copyCSS);
+exports.watch = series(copyImages, copyCSS);
