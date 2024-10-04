3a9b9110e85dce9f0858a7bb3c5401735f366563
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-10-04 14:28:32 -0700

    Removes upgrade-helper plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 // Filters
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
-const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 
 // RSS Feed plugin.
 const rssPlugin = require('@11ty/eleventy-plugin-rss');
@@ -210,6 +209,4 @@ module.exports = function(eleventyConfig) {
       output: 'dist'
     }
   };
-
-  eleventyConfig.addPlugin(UpgradeHelper);
 };
